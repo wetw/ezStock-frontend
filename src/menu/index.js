864dc1bb6077745b9c4a304ada024bb421834b95
weiting <a26007565@gmail.com>
2018-09-25 02:12:15 +0800

    Add menu

--- src/menu/index.js
+++ src/menu/index.js
@@ -4,8 +4,33 @@ export default [{
     href: '/'
   },
   {
-    title: 'about',
-    icon: 'question_answer',
-    href: '/foo'
+    title: 'cost',
+    icon: 'monetization_on',
+    href: '/cost'
+  },
+  {
+    title: 'investmentDetails',
+    icon: 'list_alt',
+    href: '/investmentDetails'
+  },
+  {
+    title: 'unrealizedProfit',
+    icon: 'show_chart',
+    href: '/unrealizedProfit'
+  },
+  {
+    title: 'realizedProfit',
+    icon: 'payment',
+    href: '/realizedProfit'
+  },
+  {
+    title: 'accountInfo',
+    icon: 'account_balance',
+    href: '/accountInfo'
+  },
+  {
+    title: 'settings',
+    icon: 'settings',
+    href: '/settings'
   }
 ]
