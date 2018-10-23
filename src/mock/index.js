b726e19e9237a89fe1286a7eb49d8960318cce62
weiting <a26007565@gmail.com>
2018-10-23 18:23:45 +0800

    Mock stock data

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 import data from './data';
 import MockAdapter from 'axios-mock-adapter';
+import stocks from './stocks.json';
 
-let mock = new MockAdapter(axios, { delayResponse: 80 });
+let mock = new MockAdapter(axios, {
+  delayResponse: 80
+});
 
 mock.onGet('/users').reply(200, {
   data: data.users.slice(0, 10)
 });
 
-export default mock;
+mock.onGet('/stocks').reply(200, {
+  data: stocks
+});
 
+mock.onGet(/\/stocks\/\w+/).reply((config) => {
+  return [200, {
+    data: stocks.find(x => x.id === config.url.split('/')[1])
+  }];
+});
+
+export default mock;
